db430e19b5029e788e87fb5f223d7dced0f5ff78
Ft-ajith <72190472+Ft-ajith@users.noreply.github.com>
2022-12-01 12:41:46 +0530

    fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  api_url: 'https://5bbx8l6uxd.execute-api.ap-south-1.amazonaws.com/v10/',
+  onboardingUrl: "https://km-dkyc-krakyc-09-dev.fintuple.in",
+  imageFileSize: 2097152,
+  applicationId: '56691624',
+  enck_Key: 'czNDcmU3S2V5NG0wOW01Rg==',
+  mfProdData:'RDR_F_P_PAN'
+  //mfProdData:'RCAS'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // api_url: 'https://seezvn05z3.execute-api.us-east-1.amazonaws.com/dev/dkyc_internal/v10/',
+  api_url: 'https://jrs8lk6sna.execute-api.ap-south-1.amazonaws.com/v10/',
+  onboardingUrl: "https://krakyc-qa.dkyc.kotak.com/",
+  imageFileSize: 999999999999999999999,
+  applicationId: '56691624',
+  enck_Key: 'czNDcmU3S2V5NG0wOW01Rg==',
+  mfProdData:'RCAS'
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
