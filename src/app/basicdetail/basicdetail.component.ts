1ab0e785280f5727c4b5fbe72ac014976e68bbae
sundar-ft <57901550+sundar-ft@users.noreply.github.com>
2022-12-01 19:12:42 +0530

    text field fixing

--- src/app/basicdetail/basicdetail.component.ts
+++ src/app/basicdetail/basicdetail.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-basicdetail',
@@ -9,5 +18,13 @@ import { Component } from '@angular/core';
 
 })
 export class BasicdetailComponent {
+  emailFormControl = new FormControl('', []);
+  panFormControl = new FormControl('', []);
+  dobFormControl = new FormControl('', []);
+  fullnameFormControl = new FormControl('', []);
+  mobileFormControl = new FormControl('', []);
+  
+  // emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
+  matcher = new MyErrorStateMatcher();
 }
--- src/app/form-validation/form-validation.component.ts
+++ src/app/form-validation/form-validation.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-form-validation',
@@ -7,5 +17,7 @@ import { Component } from '@angular/core';
 })
 export class FormValidationComponent {
    
-  
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
 } 
